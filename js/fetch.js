bdd89db5d7aff2d17c5f3b41504482ade6303f4f
stefanymelda <juwitastefany13@gmail.com>
2023-03-31 16:01:59 +0700

    latihan-fepresensi-juwita

--- js/fetch.js
+++ js/fetch.js
@@ -1,11 +1,21 @@
 import { get } from "https://bukulapak.github.io/api/process.js";
-import { setInner } from "https://bukulapak.github.io/element/process.js";
+import { addInner } from "https://bukulapak.github.io/element/process.js";
+import { getRandomColor, getRandomColorName } from "https://bukulapak.github.io/image/process.js";
+import { isiTabel } from "table.js";
 let urlAPI = "https://jaehyun.herokuapp.com/presensi";
 get(urlAPI, isiTablePresensi);
 function isiTablePresensi(results) {
-  console.log(results);
   results.forEach(isiRow);
 }
 function isiRow(value) {
-  console.log(value);
+  let content = isiTabel
+    .replace("#NAMA#", value.biodata.nama)
+    .replace("#NOHP#", value.biodata.phone_number)
+    .replace("#JABATAN#", value.biodata.jabatan)
+    .replace("#LOKASI#", value.location)
+    .replace("#STATUS#", value.checkin)
+    .replace("#HARIKERJA#", value.biodata.hari_kerja)
+    .replace("#WARNA#", getRandomColor())
+    .replace(/#WARNALOGO#/g, getRandomColorName());
+  addInner("iniTabel", content);
 }
