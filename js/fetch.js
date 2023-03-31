1bb47e4596a56ca15e3e4f9b6c1bb94d008d9464
stefanymelda <juwitastefany13@gmail.com>
2023-03-31 15:40:27 +0700

    latihan-fepresensi-juwita

--- js/fetch.js
+++ js/fetch.js
@@ -1,6 +1,11 @@
 import { get } from "https://bukulapak.github.io/api/process.js";
+import { setInner } from "https://bukulapak.github.io/element/process.js";
 let urlAPI = "https://jaehyun.herokuapp.com/presensi";
 get(urlAPI, isiTablePresensi);
 function isiTablePresensi(results) {
   console.log(results);
+  results.forEach(isiRow);
+}
+function isiRow(value) {
+  console.log(value);
 }
