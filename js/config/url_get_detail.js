7ec0acc8d992ef78fef70d85dea3fd7cb34e895f
stefanymelda <juwitastefany13@gmail.com>
2023-05-19 15:05:29 +0700

    Week10nih

---
+++ js/config/url_get_detail.js
@@ -0,0 +1,5 @@
+// Mendapatkan parameter dari URL
+const urlParams = new URLSearchParams(window.location.search);
+const presensiId = urlParams.get("presensiId");
+
+export let urlFetch = "https://jaehyun.herokuapp.com/presensi" + presensiId;
