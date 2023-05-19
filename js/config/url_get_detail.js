9d20b9ece6ca235b773945cd49d3815c04ccadab
stefanymelda <juwitastefany13@gmail.com>
2023-05-19 15:23:11 +0700

    Wekk10nih

--- js/config/url_get_detail.js
+++ js/config/url_get_detail.js
@@ -2,4 +2,4 @@
 const urlParams = new URLSearchParams(window.location.search);
 const presensiId = urlParams.get("presensiId");
 
-export let urlFetch = "https://jaehyun.herokuapp.com/presensi" + presensiId;
+export let urlFetch = "https://jaehyun.herokuapp.com/presensi/" + presensiId;
