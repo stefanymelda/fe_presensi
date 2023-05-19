4459ee9063290e4354d6be6ba0719056509efcbe
stefanymelda <juwitastefany13@gmail.com>
2023-05-19 15:56:01 +0700

    Week10nih

---
+++ js/controller/delete.js
@@ -0,0 +1,17 @@
+function deleteData() {
+  var presensiId = document.querySelector("a[data-presensi-id]").getAttribute("data-presensi-id");
+  var target_url = "https://jaehyun.herokuapp.com/delete/" + presensiId;
+
+  var requestOptions = {
+    method: "DELETE",
+    redirect: "follow",
+  };
+
+  fetch(target_url, requestOptions)
+    .then((response) => response.json())
+    .then((result) => {
+      alert(result.message);
+      location.reload();
+    })
+    .catch((error) => console.log("Error:", error));
+}
